73b07e708bcc79f2717c398b6d6e66a21db34bc5
Kaspars <kaspars.bregis@gmail.com>
2024-12-20 17:14:29 +0200

    update 20dec2024

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -2,18 +2,23 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+import BlurFade from "./ui/blur-fade";
 import { Button } from "./ui/button";
 
 const CallToAction = () => {
   return (
-    <div className="flex w-full flex-col items-center px-6">
+    <BlurFade
+      inView
+      delay={0.2}
+      className="flex w-full flex-col items-center px-6"
+    >
       <div className="mt-24 flex w-full max-w-4xl items-center justify-between rounded-3xl bg-gradient-to-r from-red-500 to-orange-500 px-6 shadow-xl">
         <div className="flex w-4/5 flex-col items-center justify-center gap-6">
-          <h1 className="section-title text-[36px] leading-tight md:px-24">
+          <h1 className="section-title text-[24px] leading-tight sm:text-[36px] md:px-24">
             Atbrīvojies no sāpēm šodien!
           </h1>
           <Link href={"/"}>
-            <Button className="w-[240px]">Pieteikties</Button>
+            <Button className="">Pieteikties</Button>
           </Link>
         </div>
         <div className="flex flex-col justify-end overflow-hidden pt-10">
@@ -25,7 +30,7 @@ const CallToAction = () => {
           />
         </div>
       </div>
-    </div>
+    </BlurFade>
   );
 };
 
---
+++ components/Vizitkarte.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Vizitkarte = () => {
+  return (
+    <div className="flex h-auto w-full max-w-5xl flex-col justify-center rounded-xl bg-red-200 p-4 text-[#292929]">
+      <h2 className="section-title text-start text-[30px]">
+        Kaspars Breģis - sertificēts fizioterapeits
+      </h2>
+    </div>
+  );
+};
+
+export default Vizitkarte;
