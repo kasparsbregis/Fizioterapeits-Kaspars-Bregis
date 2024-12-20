caecced599e8001c75909df3e4a27d8f54853590
Kaspars <kaspars.bregis@gmail.com>
2024-12-20 13:34:47 +0200

    updated CTA section

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -7,21 +7,21 @@ import { Button } from "./ui/button";
 const CallToAction = () => {
   return (
     <div className="flex w-full flex-col items-center px-6">
-      <div className="mt-24 flex max-w-[1000px] items-center justify-between rounded-3xl bg-gradient-to-r from-red-500 to-orange-500 px-6 shadow-xl">
-        <div className="flex h-[300px]  flex-col items-center justify-center gap-6">
+      <div className="mt-24 flex w-full max-w-4xl items-center justify-between rounded-3xl bg-gradient-to-r from-red-500 to-orange-500 px-6 shadow-xl">
+        <div className="flex w-4/5 flex-col items-center justify-center gap-6">
           <h1 className="section-title text-[36px] leading-tight md:px-24">
-            Atbrīvojies no sāpēm. Šodien.
+            Atbrīvojies no sāpēm šodien!
           </h1>
           <Link href={"/"}>
             <Button className="w-[240px]">Pieteikties</Button>
           </Link>
         </div>
-        <div className="flex h-[300px] flex-col overflow-hidden pt-10">
+        <div className="flex flex-col justify-end overflow-hidden pt-10">
           <Image
             src={"/kb.png"}
             alt="Kaspars Bregis"
-            width={180}
-            height={180}
+            width={150}
+            height={150}
           />
         </div>
       </div>
