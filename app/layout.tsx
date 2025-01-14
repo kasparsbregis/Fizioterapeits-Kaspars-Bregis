b514b0daecbb03d7ee0e9ad8024eeb75162106d3
Kaspars <kaspars.bregis@gmail.com>
2025-01-14 09:43:32 +0200

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Head from "next/head";
 
 const DMSans = localFont({
   src: "./fonts/DMSansVF.ttf",
@@ -9,8 +10,42 @@ const DMSans = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Fizioterapeits Kaspars Breģis",
-  description: "Sertificēts fizioterapeits Siguldā",
+  title: "Breģis Kaspars - fizioterapeita prakse",
+  description:
+    "Fizioterapeits Siguldā. Piedāvāju ārstniecisko vingrošanu, masāžu un kinezioloģisko teipošanu. Atrodos Siguldā, Pils ielā 13a. Zvani +371 26779084.",
+  keywords: [
+    "fizioterapeits",
+    "fizioterapeits Sigulda",
+    "vingrošana",
+    "vingrošana Sigulda",
+    "masāža",
+    "masāža Sigulda",
+    "teipošana",
+    "teipošana Sigulda",
+  ],
+  openGraph: {
+    title: "Breģis Kaspars - fizioterapeita prakse | Sigulda",
+    description:
+      "Fizioterapeits Siguldā. Piedāvāju ārstniecisko vingrošanu, masāžu un kinezioloģisko teipošanu. Atrodos Siguldā, Pils ielā 13a.",
+    url: "https://www.fiziokaspars.lv",
+    siteName: "Breģis Kaspars - fizioterapeita prakse",
+    images: [
+      {
+        url: "https://www.fiziokaspars.lv/assets/og-logo.png",
+        width: 1355,
+        height: 300,
+        alt: "Fizioterapija Siguldā - Breģis Kaspars",
+      },
+    ],
+    locale: "lv_LV",
+    type: "website",
+  },
+  alternates: {
+    canonical: "https://www.fiziokaspars.lv",
+    languages: {
+      "lv-LV": "https://www.fiziokaspars.lv",
+    },
+  }
 };
 
 export default function RootLayout({
@@ -20,6 +55,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        {/* JSON-LD Structured Data */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              name: "Breģis Kaspars - fizioterapeita prakse",
+              telephone: "+371 26779084",
+              url: "https://www.fiziokaspars.lv",
+              image: "https://www.fiziokaspars.lv/assets/og-image.png",
+            }),
+          }}
+        />
+      </Head>
       <body className={`${DMSans.className}  bg-[#ceecea] antialiased`}>
         {children}
       </body>
