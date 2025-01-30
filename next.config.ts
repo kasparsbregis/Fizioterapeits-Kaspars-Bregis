fe369dad7a369f4ac8ecb50ee39f36b43a79f12d
Kaspars <kaspars.bregis@gmail.com>
2025-01-30 14:55:27 +0200

    deleted next.config.ts code

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,6 @@
 import { NextConfig } from 'next'
 
 const config: NextConfig = {
-  async redirects() {
-    return [
-      // Redirect from non-www HTTP to HTTPS www
-      {
-        source: '/:path*', // Capture all paths
-        has: [
-          {
-            type: 'host',
-            value: 'fiziokaspars.lv',
-          },
-        ],
-        destination: 'https://www.fiziokaspars.lv/:path*', // Redirect to the www version
-        permanent: true,
-      },
-      // Redirect from non-www HTTPS to www
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'fiziokaspars.lv',
-          },
-        ],
-        destination: 'https://www.fiziokaspars.lv/:path*', // Redirect to the www version
-        permanent: true,
-      }
-    ]
-  }
 }
 
 export default config
