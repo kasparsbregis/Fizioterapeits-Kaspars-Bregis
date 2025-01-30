e77e18fd9dbc39b925f4782d4d6abf666454bd70
Kaspars <kaspars.bregis@gmail.com>
2025-01-30 13:45:14 +0200

    changes in next config

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,20 @@ import { NextConfig } from 'next'
 const config: NextConfig = {
   async redirects() {
     return [
-      // Redirect from http://fiziokaspars.lv or https://fiziokaspars.lv to https://www.fiziokaspars.lv
+      // Redirect from non-www HTTP to HTTPS www
       {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'fiziokaspars.lv',  // Target domain without www
-          },
-        ],
-        destination: 'https://www.fiziokaspars.lv/:path*',  // Redirect to www with HTTPS
-        permanent: true,  // Use 301 (permanent) redirect
+        source: 'http://fiziokaspars.lv',
+        destination: 'https://www.fiziokaspars.lv',
+        permanent: true,
       },
+      // Redirect from non-www HTTPS to www
+      {
+        source: 'https://fiziokaspars.lv',
+        destination: 'https://www.fiziokaspars.lv',
+        permanent: true,
+      }
     ]
-  },
+  }
 }
 
 export default config
